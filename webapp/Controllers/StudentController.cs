88ab1d3a414545483b4c99da4efeaacbad640044
asp dotnet <asp.net1345@bellurbis.com>
2023-01-28 21:24:46 +0530

    Add project files.

---
+++ webapp/Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.PortableExecutable;
+using webapp.Models;
+
+namespace webapp.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentContext db;
+        public StudentController(StudentContext _db)
+        {
+            db = _db;        
+        }
+        public IActionResult Index()
+        {
+            var list =  db.Tb_Student.ToList();
+            return View(list);
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(StudentModel std)
+        {
+            if (std !=null)
+            {
+                if (std.StudentId == 0)
+                {
+                    db.Tb_Student.Add(std);                    
+                }
+                else
+                {
+                    db.Entry(std).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                }
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id) 
+        {
+            var obj = db.Tb_Student.Find(id);
+            return View(obj);
+        }
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var obj = db.Tb_Student.Find(id);
+            db.Remove(obj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ webapp/Models/StudentModel.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace webapp.Models
+{
+    public class StudentModel
+    {
+        [Key]
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int RollNo { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public StudentContext(DbContextOptions<StudentContext> options) : base(options)
+        {
+
+        }
+        public DbSet<StudentModel> Tb_Student { get; set; }
+    }
+
+}
